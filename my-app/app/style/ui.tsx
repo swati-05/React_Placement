e030566716cab3a237af1fe48498abb8aef4004d
Swati Sharma <swatisharma52000@gmail.com>
2025-12-18 19:30:25 +0530

    about page

---
+++ my-app/app/style/ui.tsx
@@ -0,0 +1,5 @@
+export const buttonPrimary = "px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition";
+
+export const heading = "text-3xl font-bold text-center text-red-500 underline";  
+ 
+export const paraText  = "p-6 text-gray-600 leading-6 text-sm";
